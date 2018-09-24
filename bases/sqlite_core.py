bcd16bb8d86cf99ec5468b3d4d34ecfb47742c07
irip <irip@bk.ru>
2018-09-25 00:16:29 +0300

    http://www.cyberforum.ru/post12921496.html

--- bases/sqlite_core.py
+++ bases/sqlite_core.py
@@ -3,8 +3,14 @@ import sqlite3
 
 class sqlite_base:
     def __init__(self, name_db):
+        """
+            Запись результата в базу данных
+        """
+        # Connection объект олицетворяет базу данных
         self.conn = sqlite3.connect(name_db)
+        # Создание таблицы
         self.c = self.conn.cursor()
+        # Вставка ряда данных
         self.c.execute(
                 '''
                 CREATE TABLE IF NOT EXISTS vkdata( 
@@ -19,6 +25,8 @@ class sqlite_base:
         #     print("Не удалось создать таблицу")
 
     def save(self, data):
-        self.c.executemany("INSERT INTO vkdata VALUES (?,?,?)", data)
+        self.c.executemany("INSERT OR IGNORE INTO vkdata(id) VALUES (?,?,?)", data)
+        # Сохранение (commit) изменений
         self.conn.commit()
+        # Закрытие курсора, в случае если он больше не нужен
         self.conn.close()
