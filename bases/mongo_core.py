3aba1a97bcba1349993d43c7fa29f4bb0404ac30
irip <irip@bk.ru>
2018-09-30 12:24:00 +0300

    2 замещаем sqlite3 на mongodb для хранения временных данных. работа с оперативной памятью

--- bases/mongo_core.py
+++ bases/mongo_core.py
@@ -2,3 +2,19 @@
 
 import pymongo
 
+
+class sqlite_base:
+    def __init__(self, name_db):
+        """
+            Запись результата в базу данных
+        """
+        self.conn = pymongo.MongoClient(name_db)
+
+
+    def save(self, data):
+        """
+            Запись строки в mongodb
+        :param data:
+        :return:
+        """
+        pass
